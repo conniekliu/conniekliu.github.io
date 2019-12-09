eb802b03b88eafeede9ac059e5558bf83f809e7e
conniekliu <conniekliu95@gmail.com>
2019-12-08 17:20:09 -0800

    add new website

---
+++ js/responsive.js
@@ -0,0 +1,41 @@
+function menubutton(resizeLimit) {
+	// Handle default parameter
+	resizeLimit = typeof resizeLimit !== 'undefined' ? resizeLimit : 800;
+	if ($(window).width() <= resizeLimit) {
+		$('.menu-icon').show()
+		$('.dropdown-menu').hide()
+		$('.dropdown-menu').css('z-index', '1001')
+		$('.navbar').hide()
+	} else if ($(window).width() > resizeLimit) {
+		$('.menu-icon').hide()
+		$('.dropdown-menu').hide()
+		$('.dropdown-menu').css('z-index', '-1001')
+		$('.navbar').show()
+}
+};
+
+function resize(div, resizeLimit) {
+	$(div).each(function() {
+	if ($(this).width() <= parseInt($(this).css("min-width"))) {
+			$(this).css({"display":"contents"});
+		} else {
+			$(this).css({"display":"table-cell"});
+		}
+	});
+};
+
+function resizesocial(div, resizeLimit) {
+	resizeLimit = typeof resizeLimit !== 'undefined' ? resizeLimit : 800;
+	if ($(window).width() <= resizeLimit) {
+		$(div).css({"display":"block"});
+	} else if ($(window).width() > resizeLimit) {
+		$(div).css({"display":"table-cell"});
+	}
+};
+
+
+function toppage() {
+	var header_size = $('header').offset().top + ($('header').outerHeight()*1.5);
+	$('.main-wrapper').css({'padding-top': header_size});
+}
+
